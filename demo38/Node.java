f632b914b3d61d7448b1c24c2d23edcaaf6578b5
刘妍 <1712239475@qq.com>
2021-06-26 19:10:58 +0800

    Myhashmap

---
+++ demo38/Node.java
@@ -0,0 +1,13 @@
+package data66;
+
+public class Node {
+    Integer key;
+    Integer value;
+
+    Node next;
+
+    public Node(Integer key, Integer value) {
+        this.key = key;
+        this.value = value;
+    }
+}
